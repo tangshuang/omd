dbff60adcc34fdbcd6d090ff2e0c8d1a1e924ae4
tison <ts@ajrice.com>
2016-01-21 10:09:32 +0800

    调整了requirejs，使它感觉像依赖加载，从而优化plugin.omd.js

--- demo.plugin.omd.js
+++ demo.plugin.omd.js
@@ -1,18 +1,15 @@
 !function(fun){
     "use strict";
-    function __() {
-        return $;
-    }
 
     // 当define被定义的情况下
     if(typeof define == 'function' && (define.amd != undefined || define.cmd != undefined)) {
-        define(function(){
-            return fun(__());
+        define(function() {
+            return fun($);
         });
     }
     // 当define没有被定义的情况下
     else {
-        var ex = fun(__());
+        var ex = fun($);
         // CommonJS NodeJS
         if (typeof module !== 'undefined' && typeof exports === 'object') {
             // 由于exports被定义，函数中的exports已经是全局变量，因此，这里就不进行任何操作
