3622cc836b33180c12c2b1655bf44277a823c419
tangshuang <mail@tangshuang.net>
2016-02-17 23:23:14 +0800

    Update omd.js

--- omd.js
+++ omd.js
@@ -1,13 +1,21 @@
 !function(factory){
     "use strict";
     
-    if(typeof $ === 'undefined') var $ = {};
+    var spacename = 'yourfilename'; // 改为文件名（去掉后缀），作为本模块的唯一标识
     
-    // 当define被定义的情况下
+    if(typeof $ === 'undefined') 
+        var $ = {};
+    
+    // 当define被定义的情况下 RequireJS SeaJS
     if(typeof define == 'function' && (define.amd != undefined || define.cmd != undefined)) {
         define(function() {
             return factory($);
         });
+        /* 如果在写一个requrejs的模块，可以考虑下面这种方式，这样可以提前把jquery加载进来，防止jquery无法使用
+        define(spacename,['jquery'],function(){
+            return factory($);
+        });
+        */
     }
     // 当define没有被定义的情况下
     else {
@@ -17,11 +25,9 @@
             // 由于exports被定义，函数中的exports已经是全局变量，因此，这里就不进行任何操作
             module.exports = ex;
         }
-        // Javascript，接口将直接被作为一个函数
+        // 原生Javascript，接口将被作为一个window的子对象
         else {
-            for(var i in ex) {
-                window[i] = ex[i];
-            }
+            window[spacename] = ex;
         }
     }
 }(function($){
