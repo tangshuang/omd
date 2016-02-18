3572d6e400cd5ddc11ce06e1e92282fbe080fc3d
tangshuang <mail@tangshuang.net>
2016-02-18 21:08:19 +0800

    Delete main.js

--- requirejs/main.js
+++
@@ -1,7 +0,0 @@
-define(['jquery'],function($){
-  // 由于plugin依赖于jquery，所以不得不按照这样的格式进行使用
-  require(['plugin'],function($plugin){
-    $('#test1').fillText('插件模式调用');
-    $plugin.fillText('#test2','接口模式调用');
-  });
-});
