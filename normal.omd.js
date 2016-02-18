ae1774ff3b33dfd7bb97461ca3493119d205ed6e
tangshuang <mail@tangshuang.net>
2016-02-18 16:31:33 +0800

    Update normal.omd.js

--- normal.omd.js
+++ normal.omd.js
@@ -1,16 +1,14 @@
+/**
+ * omd 让你写的javascript代码兼容所有的运行环境，符合amd, cmd, commonjs规范，在原生环境中也能运行
+ * 例如，你写了一堆代码，在没有模块化加载的时候可以使用，在模块化框架下也可以使用
+ */
+
 !function(spacename,dependencies,window,factory){
     // 当define被定义的情况下
-    if(typeof define == 'function') {
-        if(define.amd != undefined) {
-            define(dependencies,function() {
-                return factory(window,require);
-            });
-        }
-        else if(define.cmd != undefined) {
-            define(dependencies,function(require) {
-                return factory(window,require);
-            });
-        }
+    if(typeof define == 'function' && (define.amd != undefined || define.cmd != undefined)) {
+        define(dependencies,function() {
+            return factory(window);
+        });
     }
     // 当define没有被定义的情况下
     else {
@@ -25,7 +23,7 @@
             window[spacename] = ex;
         }
     }
-}('spaceName',['jquery'],window,function(window,require){
+}('spaceName',['jquery'],window,function(window){
     var $ = (window.$ != undefined ? window.$ : null);
     // var $ = require('jquery');
 
@@ -35,8 +33,7 @@
      * 2. 修改上面['jquery']的内容为依赖包列表【在模块化环境中可能使用】
      * 3. window就是window，有你需要的window属性
      * 4. 加载$，如果你的项目中依赖了jQuery或Zepto，则选择上面注释中的一种，使$可用
-     * 5. 模块加载，像requirejs或seajs一样，使用require
-     * 6. 接口，通过return返回接口
+     * 5. 接口，通过return返回接口
      */
      
 });
